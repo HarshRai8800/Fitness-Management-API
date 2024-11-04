4d2869ae1ef95aeb6a4c0f0a0da524864d2cf987
Harsh Rai <153977193+HarshRai8800@users.noreply.github.com>
2024-11-04 12:43:53 +0530

    Update Mongoos.js

--- momgoose/Mongoos.js
+++ momgoose/Mongoos.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose")
 
 
-mongoose.connect("mongodb+srv://Intern:1234@cluster0.ct1ii.mongodb.net/")
+mongoose.connect("enter your mongodb compass url")
 
 module.exports = mongoose
